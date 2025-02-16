de1946963e89f94db25c421dc8f83feb8aa39829
Titoan <ryb.anton.le@gmail.com>
2025-02-16 10:11:24 +0200

    dell Controller imports

--- src/controllers/gameController.ts
+++ src/controllers/gameController.ts
@@ -1,10 +1,4 @@
-import readline from "readline";
-import {rooms, actions } from "../models/gameModel";
-import gameView from "../views/gameView";
-import { Room, RoomsType } from "../models/gameModel";
-import { MoveCommand, ActionCommand, ExitCommand } from "../commands/Commands";
 import Game from "../models/gameClass";
-
 
 export default class Cotnroller {    
     
